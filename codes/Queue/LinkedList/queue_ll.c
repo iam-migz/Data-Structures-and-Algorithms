ca13d785543a4c3c39b06ce17aae33cf3d81fcdf
miguel <mareyes2132@gmail.com>
2022-03-18 22:04:09 +0800

    queue_ll insert sorted

--- codes/Queue/LinkedList/queue_ll.c
+++ codes/Queue/LinkedList/queue_ll.c
@@ -53,9 +53,87 @@ int dequeue(Queue *q) {
     return retval;
 }
 
+void display_queue(Queue q) {
+    while (q.front != NULL) {
+        printf("%d ", q.front->data);
+        q.front = q.front->next;
+    }
+}
 
+// using a temp queue
+void insert_sorted(Queue *q, int data) {
+    Queue c;
+    NodePtr temp;
+    init_queue(&c);
+    
+    while (q->front != NULL && data > q->front->data) {
+        temp = q->front;
+
+        // dequeue in the orig. queue
+        q->front = temp->next;
+
+        // enqueue to the temp queue
+        if (c.front == NULL) {
+            c.front = temp;
+        } else {
+            c.back->next = temp;
+        }
+        c.back = temp;
+    }
+
+    // *insert_last to the new queue 
+    temp = malloc(sizeof(NodeType));
+    if (temp != NULL) {
+        temp->data = data;
+        temp->next = NULL;
+        if (c.back == NULL) {
+            c.front = temp;
+        } else {
+            c.back->next = temp;
+        }
+        c.back = temp;
+    }
+
+    // empty the orig queue if there is any left
+    while (q->front != NULL) {
+        temp = q->front;
+        q->front = temp->next;
+
+        c.back->next = temp;
+        c.back = temp;
+    }
+
+    // return all contents back to the orig. queue
+    while (c.front != NULL) {
+        temp = c.front;
+        c.front = temp->next;
+        if (q->front == NULL) {
+            q->front = temp;
+        } else {
+            q->back->next = temp;
+        }
+        q->back = temp;
+    }
+}
+
+
+void ll_make_null(NodePtr *L) {
+    NodePtr temp;
+    while (*L != NULL) {
+        temp = *L;
+        *L = (*L)->next;
+        free(temp);
+    }
+}
 
 int main() {
 
+    Queue q;
+    init_queue(&q);
+    enqueue(&q, 2);
+    enqueue(&q, 4);
+    enqueue(&q, 6);
+    insert_sorted(&q, 5);
+    ll_make_null(&q.front);
     return 0;
 }
